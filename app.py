1f50200fe0485251435fbc88e27df4df6c5acf09
Casey Johnson <ctj0001@mix.wvu.edu>
2020-04-13 14:09:46 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import env
+from flask import Flask, request, session
+from twilio.twiml.message_response import MessagingResponse
+
+app = Flask(__name__)
+app.config.update(
+    SECRET_KEY = env.APP_SECRET_KEY,
+)
+
+@app.route('/sms', methods=['POST'])
+def sms_reply():
+    player_number = request.form['From']
+    resp = MessagingResponse()
+    resp.message("Hello, {}!".format(player_number))
+    return str(resp)
+
+if __name__ == '__main__':
+    app.run()
---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+APP_SECRET_KEY = os.environ['APP_SECRET_KEY']
