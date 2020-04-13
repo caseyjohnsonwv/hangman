395638da8afd5de3c1b4ebaa6776dc32ab252891
Casey Johnson <ctj0001@mix.wvu.edu>
2020-04-13 16:21:24 -0400

    integrate serialization into app

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import env
+from hangman import *
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
+
 
 app = Flask(__name__)
 app.config.update(
     SECRET_KEY = env.APP_SECRET_KEY,
 )
+
+
+def save_game(game):
+    session['gameData'] = game.to_json()
+def load_game():
+    return HangmanGame.from_json(session['gameData'])
+
 
 @app.route('/sms', methods=['POST'])
 def sms_reply():
@@ -13,6 +22,7 @@ def sms_reply():
     resp = MessagingResponse()
     resp.message("Hello, {}!".format(player_number))
     return str(resp)
+
 
 if __name__ == '__main__':
     app.run()
